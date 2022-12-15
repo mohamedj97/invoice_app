e419eb899d330d0780af0fa576f48ee7ed215110
Mohamed Gamal <MohamedGamal.std@cis.asu.edu.eg>
2022-12-15 22:35:41 +0200

    adding AddProduct feature staff

--- lib/features/products/api/products_client.dart
+++ lib/features/products/api/products_client.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 
 import '../../../core/api/base_api_response.dart';
+import '../../../core/api/repository/memory_repo.dart';
 import '../../../core/strings/end_points.dart';
 import '../data/models/requests/product_request_model.dart';
 import '../data/models/responses/get_products_response_model.dart';
