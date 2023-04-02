85d314b7d3b0bda06fffc725bff1ab7c820af7a2
Mohamed Gamal <MohamedGamal.std@cis.asu.edu.eg>
2023-04-03 00:04:24 +0200

    add customers filter staff

---
+++ lib/features/customers/data/models/requests/get_customers_request_model.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_customers_request_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CustomerFilterGenericFilterModel _$CustomerFilterGenericFilterModelFromJson(
+        Map<String, dynamic> json) =>
+    CustomerFilterGenericFilterModel(
+      sortBy: json['sortBy'] as String?,
+      sortDir: json['sortDir'] as String?,
+      filter: json['filter'] == null
+          ? null
+          : CustomerFilter.fromJson(json['filter'] as Map<String, dynamic>),
+      pageNo: json['pageNo'] as int,
+      pageSize: json['pageSize'] as int,
+    );
+
+Map<String, dynamic> _$CustomerFilterGenericFilterModelToJson(
+        CustomerFilterGenericFilterModel instance) =>
+    <String, dynamic>{
+      'filter': instance.filter,
+      'pageNo': instance.pageNo,
+      'pageSize': instance.pageSize,
+      'sortBy': instance.sortBy,
+      'sortDir': instance.sortDir,
+    };
